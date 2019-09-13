decaac9fa26664ef335b34e92b2c4a26d6a4fe91
km323 <miyakm323@gmail.com>
2019-09-13 17:52:41 +0900

    ThirdPersonPlayerMove

---
+++ Assets/ThirdPersonPlayerRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonPlayerRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform fowardPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(fowardPos.forward);
+    }
+}
