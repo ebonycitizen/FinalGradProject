8d741b3b133f0ad726072618ae6b7b9e1fc07b58
km323 <miyakm323@gmail.com>
2019-09-19 12:17:49 +0900

    魚群

---
+++ Assets/Script/Boids/Simulation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+
+    [SerializeField]
+    int boidCount = 10;
+    [SerializeField]
+    GameObject boidPrefab;
+
+    [SerializeField]
+    Param param;
+
+    List<Boid> boids_ = new List<Boid>();
+    public ReadOnlyCollection<Boid> boids
+    {
+        get { return boids_.AsReadOnly(); }
+    }
+
+    void AddBoid()
+    {
+        var go = Instantiate(boidPrefab, Random.insideUnitSphere+transform.position, Random.rotation);
+        go.transform.SetParent(transform);
+        var boid = go.GetComponent<Boid>();
+        boid.simulation = this;
+        boid.param = param;
+        boid.wallPos = transform.position;
+        boids_.Add(boid);
+    }
+
+    void RemoveBoid()
+    {
+        if (boids_.Count == 0) return;
+
+        var lastIndex = boids_.Count - 1;
+        var boid = boids_[lastIndex];
+        Destroy(boid.gameObject);
+        boids_.RemoveAt(lastIndex);
+    }
+
+    void Update()
+    {
+        while (boids_.Count < boidCount)
+        {
+            AddBoid();
+        }
+        while (boids_.Count > boidCount)
+        {
+            RemoveBoid();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!param) return;
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * param.wallScale);
+    }
+}
