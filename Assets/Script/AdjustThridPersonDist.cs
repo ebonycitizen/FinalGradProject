8fdd467e106e6a83080fb6c392076e9ca65bff69
ebonyCitizen <coco_coco87@hotmail.com>
2019-09-21 15:57:27 +0900

    あたり判定・改

--- Assets/Script/AdjustThridPersonDist.cs
+++ Assets/Script/AdjustThridPersonDist.cs
@@ -29,53 +29,8 @@ public class AdjustThridPersonDist : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Collider[] c = Physics.OverlapBox(thirdPersonSize.bounds.center, thirdPersonSize.bounds.size, Quaternion.identity,1 << 17);
-        //if(c.Length!=0)
-        //Debug.Log(c[0].name);
 
-
-        //if (c.Length != 0 && hit.collider == c[0])
-        //    transform.localPosition = Vector3.Slerp(transform.localPosition, originAxis.transform.InverseTransformPoint(originAxis.position + originAxis.forward * hit.distance), lerpSpeed * Time.deltaTime);
-
-        //if(!isHit || c.Length == 0)
-        //    transform.localPosition = Vector3.Slerp(transform.localPosition, normalDist, lerpSpeed * Time.deltaTime);
-
-        ////transform.localPosition = new Vector3(0, 0, transform.localPosition.z);
-        //CheckCollision();
-
-
-        if (isHit)
-        {
-            transform.position = Vector3.Slerp(transform.position, originAxis.position + rayDirection * hit.distance, lerpSpeed * Time.deltaTime);
-        }
-        //else
-        //{
-        //    transform.localPosition = Vector3.Slerp(transform.localPosition, normalDist, lerpSpeed * Time.deltaTime);
-        //}
     }
-
-    private void CheckCollision()
-    {
-        rayDirection = (transform.position - originAxis.position).normalized;
-
-        //isHit = Physics.Raycast(originAxis.position, originAxis.forward, out hit, rayDistance, 1 << LayerMask.NameToLayer("Stage"));
-        isHit = Physics.BoxCast(originAxis.position, thirdPersonSize.bounds.extents, rayDirection, out hit, Quaternion.identity, rayDistance, 1 << 17);
-        if (isHit)
-        {
-
-            //Debug.Log(hit.collider.contactOffset);
-            // Debug.Log(hit.collider.ClosestPoint(transform.position));
-            //Debug.Log(hit.normal.normalized);
-            //Debug.Log((hit.collider.ClosestPoint(transform.position) - hit.transform.position).normalized);
-        }
-        
-    }
-
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    Debug.Log(other.ClosestPoint(transform.position));
-
-    //}
 
     private void OnCollisionStay(Collision collision)
     {
@@ -84,21 +39,52 @@ public class AdjustThridPersonDist : MonoBehaviour
 
         ContactPoint[] c = collision.contacts;
 
-        Debug.Log(c[0].normal + " " + c[0].separation);
+        //Debug.Log(c[0].normal + " " + c[0].separation);
         transform.position = transform.position + c[0].normal * -c[0].separation;
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(thirdPersonSize.bounds.center, thirdPersonSize.bounds.size);
+    #region gomi
+    //private void CheckCollision()
+    //{
+    //    rayDirection = (transform.position - originAxis.position).normalized;
 
-        if (isHit)
-        {
-            Gizmos.DrawRay(originAxis.position, originAxis.forward * hit.distance);
-            Gizmos.DrawWireCube(originAxis.position + originAxis.forward * hit.distance, thirdPersonSize.bounds.size);
-            //Debug.Log(originAxis.transform.InverseTransformPoint(hit.point));
-        }
-        else
-            Gizmos.DrawRay(originAxis.position, originAxis.forward * rayDistance);
-    }
+    //    isHit = Physics.Raycast(originAxis.position, originAxis.forward, out hit, rayDistance, 1 << LayerMask.NameToLayer("Stage"));
+    //    isHit = Physics.BoxCast(originAxis.position, thirdPersonSize.bounds.extents, rayDirection, out hit, Quaternion.identity, rayDistance, 1 << 17);
+    //    if (isHit)
+    //    {
+
+    //        Debug.Log(hit.collider.contactOffset);
+    //        Debug.Log(hit.collider.ClosestPoint(transform.position));
+    //        Debug.Log(hit.normal.normalized);
+    //        Debug.Log((hit.collider.ClosestPoint(transform.position) - hit.transform.position).normalized);
+    //    }
+
+    //}
+
+    /*UPDATE*/
+
+    //Collider[] c = Physics.OverlapBox(thirdPersonSize.bounds.center, thirdPersonSize.bounds.size, Quaternion.identity,1 << 17);
+    //if(c.Length!=0)
+    //Debug.Log(c[0].name);
+
+
+    //if (c.Length != 0 && hit.collider == c[0])
+    //    transform.localPosition = Vector3.Slerp(transform.localPosition, originAxis.transform.InverseTransformPoint(originAxis.position + originAxis.forward * hit.distance), lerpSpeed * Time.deltaTime);
+
+    //if(!isHit || c.Length == 0)
+    //    transform.localPosition = Vector3.Slerp(transform.localPosition, normalDist, lerpSpeed * Time.deltaTime);
+
+    ////transform.localPosition = new Vector3(0, 0, transform.localPosition.z);
+    //CheckCollision();
+
+
+    //if (isHit)
+    //{
+    //    transform.position = Vector3.Slerp(transform.position, originAxis.position + rayDirection * hit.distance, lerpSpeed * Time.deltaTime);
+    //}
+    //else
+    //{
+    //    transform.localPosition = Vector3.Slerp(transform.localPosition, normalDist, lerpSpeed * Time.deltaTime);
+    //}
+    #endregion
 }
--- Assets/Script/ThirdPersonPlayerPosition.cs
+++ Assets/Script/ThirdPersonPlayerPosition.cs
@@ -10,12 +10,12 @@ public class ThirdPersonPlayerPosition : MonoBehaviour
     [SerializeField]
     private float distance;
 
-    ContactPoint[] c;
+    private ContactPoint[] c;
+
     private GameObject collideTarget;
     private Vector3 targetPos;
-    private Vector3 test;
-
     private Vector3 targetPosOld;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,39 +32,9 @@ public class ThirdPersonPlayerPosition : MonoBehaviour
     {
         targetPos = Quaternion.Euler(target.eulerAngles) * transform.forward * distance;
 
-        //transform.localPosition = velocity;
-
-        Vector3 moveDirection = (targetPos - targetPosOld).normalized;
-        Debug.Log(moveDirection);
-
-        if (collideTarget != null)
-        {
-            foreach (ContactPoint ca in c)
-            {
-                if (ca.normal.x != 0)
-                {
-                    if ((ca.normal.x == 1 && moveDirection.x < 0) || (ca.normal.x == -1 && moveDirection.x > 0))
-                        targetPos.x = targetPosOld.x;
-                }
-                if (ca.normal.y != 0)
-                {
-                    if ((ca.normal.y == 1 && moveDirection.y < 0) || (ca.normal.y == -1 && moveDirection.y > 0))
-                        targetPos.y = targetPosOld.y;
-                }
-                if (ca.normal.z != 0)
-                {
-                    if ((ca.normal.z == 1 && moveDirection.z < 0) || (ca.normal.z == -1 && moveDirection.z > 0))
-                        targetPos.z = targetPosOld.z;
-                }
-            }
-            //&& moveDirection.x <= 0)
-            //velocity.x = velocityOld.x;
-        }
 
         transform.localPosition = Vector3.Slerp(transform.localPosition, targetPos, Time.deltaTime * 2f);
-        //transform.localPosition = velocity;
-        Debug.Log(targetPosOld);
-        targetPosOld = targetPos;
+        //targetPosOld = targetPos;
     }
 
     private void OnCollisionStay(Collision collision)
@@ -72,12 +42,36 @@ public class ThirdPersonPlayerPosition : MonoBehaviour
         if (collision.gameObject.layer != LayerMask.NameToLayer("Stage"))
             return;
 
-        collideTarget = collision.gameObject;
-        c = collision.contacts;
+        ContactPoint[] c = collision.contacts;
+
+        //Debug.Log(c[0].normal + " " + c[0].separation);
+        transform.position = transform.position + c[0].normal * -c[0].separation;
     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        collideTarget = null;
-    }
+    #region gomi
+
+    //Vector3 moveDirection = (targetPos - targetPosOld).normalized;
+
+    //if (collideTarget != null)
+    //{
+    //    foreach (ContactPoint ca in c)
+    //    {
+    //        if (ca.normal.x != 0)
+    //        {
+    //            if ((ca.normal.x > 0 && moveDirection.x < 0) || (ca.normal.x < 0 && moveDirection.x > 0))
+    //                targetPos.x = targetPosOld.x;
+    //        }
+    //        if (ca.normal.y != 0)
+    //        {
+    //            if ((ca.normal.y > 0 && moveDirection.y < 0) || (ca.normal.y < 0 && moveDirection.y > 0))
+    //                targetPos.y = targetPosOld.y;
+    //        }
+    //        if (ca.normal.z != 0)
+    //        {
+    //            if ((ca.normal.z > 0 && moveDirection.z < 0) || (ca.normal.z < 0 && moveDirection.z > 0))
+    //                targetPos.z = targetPosOld.z;
+    //        }
+    //    }
+    //}
+    #endregion
 }
