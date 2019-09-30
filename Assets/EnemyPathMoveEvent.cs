335f3ab2bdb1777ce290eca65c101ce0561cdbdd
km323 <miyakm323@gmail.com>
2019-09-30 15:50:37 +0900

    whale

--- Assets/EnemyPathMoveEvent.cs
+++ Assets/EnemyPathMoveEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class EnemyPathMove : MonoBehaviour
+public class EnemyPathMoveEvent : MonoBehaviour
 {
     [SerializeField]
     private float moveTime;
@@ -17,22 +17,37 @@ public class EnemyPathMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] movePath = new Vector3[pathRef.childCount-1];
+        Vector3[] movePath = new Vector3[pathRef.childCount - 1];
 
-        for(int i=0;i< movePath.Length;i++)
+        for (int i = 0; i < movePath.Length; i++)
         {
             movePath[i] = pathRef.GetChild(i).localPosition;
         }
 
         transform.DOLocalPath(movePath, moveTime, PathType.CatmullRom)
            .SetEase(ease).SetDelay(delayTime).SetLookAt(0.05f, Vector3.forward)
-           .OnComplete(() => DestroyObj()).GotoWaypoint(0,true);
+           .OnComplete(() => DestroyObj()).GotoWaypoint(0, true);
+
+        StartCoroutine("rotate");
+    }
+
+    IEnumerator rotate()
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        while(transform.eulerAngles.z<350)
+        {
+            transform.eulerAngles += new Vector3(0, 0, 1);
+            yield return null;
+        }
+
+        yield break;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void DestroyObj()
